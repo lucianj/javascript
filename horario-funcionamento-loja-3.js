4515a3cd2658eb302ad6a2fd57bf4165dc792559
Lucian <lucianjosegoncales@gmail.com>
2025-11-07 17:20:05 -0300

    Atualização da solução.

--- horario-funcionamento-loja-3.js
+++ horario-funcionamento-loja-3.js
@@ -2,30 +2,9 @@
 //const diaSemana = 0;
 const diaSemana = 2;
 
-switch(diaSemana){
-    case 0:
-       console.log("A loja está aberta em horário especial: 10h às 14h.");
-       break;
-    case 1:
-       console.log("A loja está aberta no horário normal: 9h às 18h.");
-       break;
-    case 2:
-        console.log("A loja está aberta no horário normal: 9h às 18h.");
-        break;
-    case 3:
-        console.log("A loja está aberta no horário normal: 9h às 18h.");
-        break;
-    case 4:
-        console.log("A loja está aberta no horário normal: 9h às 18h.");
-        break;
-    case 5:
-        console.log("A loja está aberta no horário normal: 9h às 18h.");
-        break;
-    case 6:
-        console.log("A loja está aberta em horário especial: 10h às 14h.");
-        break;
-    default:
-        console.log("Opção não encontrada");
-        break;
 
+if (diaSemana === 6 || diaSemana === 0) {
+  console.log("A loja está aberta em horário especial: 10h às 14h.");
+} else {
+  console.log("A loja está aberta no horário normal: 9h às 18h.");
 }
