8e1292d0b8b13e7ebf58da1148ea2485837b5dc1
Lucian <lucianjosegoncales@gmail.com>
2025-11-07 16:55:25 -0300

    Classifica temperatura

---
+++ classifica-temperatura-1.js
@@ -0,0 +1,11 @@
+let temp = 10
+//let temp = 22
+//let temp = 30
+
+if (temp > 25){
+    console.log("Quente");
+}else if ((temp >=15) && (temp <=25)){
+    console.log("Agradável");
+}else {
+    console.log("Frio");
+}
