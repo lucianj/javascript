f3a41cc10b04847ce70c8a8748660a83ab233d0b
Lucian <lucianjosegoncales@gmail.com>
2025-11-07 17:02:04 -0300

    classificar idade atividade

---
+++ classifica-idade-atividade-2.js
@@ -0,0 +1,11 @@
+//const idade = 8;
+//const idade = 15;
+//const idade = 20;
+
+if (idade < 12){
+    console.log("Infantil");
+}else if ((idade >= 12) && (idade < 18)){
+    console.log("Juvenil");
+}else{
+    console.log("Adulto");
+}
