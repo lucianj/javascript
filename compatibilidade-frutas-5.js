65075927dadbb0c12f5fbdaaf6df6d87cc261e25
Lucian <lucianjosegoncales@gmail.com>
2025-11-10 08:36:04 -0300

    Exercício 5 - compatibilidade frutas

---
+++ compatibilidade-frutas-5.js
@@ -0,0 +1,7 @@
+const fruta = 'beringela';
+
+if ((fruta === 'laranja') || (fruta === 'abacaxi')){
+   console.log("Fruta compatível para a receita.");
+} else {
+    console.log("Fruta incompatível para a receita.");
+}
