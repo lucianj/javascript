8337fbcaa238952d7913e12d1404fec52f15e9d7
Lucian <lucianjosegoncales@gmail.com>
2025-11-10 08:57:53 -0300

    Exercício 8 -  verficar acesso jogo

---
+++ verificar-acesso-jogo-8.js
@@ -0,0 +1,8 @@
+const pontos = 50;
+const vidas = 1;
+
+if ((pontos >= 50) && (vidas >= 1)){
+    console.log("Próximo nível liberado");
+}else{
+    console.log("Não pode avançar para o próximo nível.");
+}
