750f52eabbf26ee3c1072d5a78f522cdde4084fc
Lucian <lucianjosegoncales@gmail.com>
2025-11-10 10:26:13 -0300

    Exercício 7 adatado usando o do - while.

--- somando-primeiros-n-numeros-7.js
+++ somando-primeiros-n-numeros-7.js
@@ -1,7 +1,15 @@
 const totalDias = 10;
-let somaEconomia = 0;
+let economia = 0;
+let dia = 1;
 
-for (let i=1; i<=totalDias; i++){
-    somaEconomia += i;
-    console.log("Dia", i + ":","R$" + somaEconomia);
-}
+do {
+  economia += dia;
+  dia++;
+} while (dia <= totalDias);
+ 
+console.log(`Total economizado: R$ ${economia}`);
+
+//for (let i=1; i<=totalDias; i++){
+//    somaEconomia += i;
+//    console.log("Dia", i + ":","R$" + somaEconomia);
+//}
