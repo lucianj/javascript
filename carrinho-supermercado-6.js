7f658456e491d7aaa1b07aa79ea3be81181de8f8
Lucian <lucianjosegoncales@gmail.com>
2025-11-10 08:43:15 -0300

    Exercício 6 - carrinho

---
+++ carrinho-supermercado-6.js
@@ -0,0 +1,7 @@
+const carrinhoVazio = false;
+
+if (!carrinhoVazio) {
+  console.log("Compra finalizada com sucesso!");
+} else {
+  console.log("Não é possível finalizar a compra: carrinho vazio.");
+}
