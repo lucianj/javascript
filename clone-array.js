8d2060ed754056e049328affa1b469272d72f732
Lucian <lucianjosegoncales@gmail.com>
2025-11-11 13:52:40 -0300

    Exercício 6 - Curso 4 - javascript - arrays.

---
+++ clone-array.js
@@ -0,0 +1,14 @@
+const pedidos = ['camiseta', 'calça', 'tênis'];
+
+
+const copiaPedidos = pedidos.slice();
+//const copiaPedidos = [];
+
+//for (let i in pedidos){
+//    copiaPedidos.push(pedidos[i]);
+//}
+
+copiaPedidos.push('boné');
+
+console.log(pedidos);
+console.log(copiaPedidos);
