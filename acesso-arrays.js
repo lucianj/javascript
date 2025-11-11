f6e2d080207ca86f6419541a90d2898e15dadb66
Lucian <lucianjosegoncales@gmail.com>
2025-11-11 11:25:21 -0300

    Exercício 1 - Curso 4 - javascript - arrays

---
+++ acesso-arrays.js
@@ -0,0 +1,6 @@
+const listaDeCompras = ['arroz', 'feijão', 'macarrão', 'tomate'];
+
+console.log(listaDeCompras[1]);
+listaDeCompras[listaDeCompras.length - 1] = 'fruta';
+
+console.log(listaDeCompras);
