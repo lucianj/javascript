bb4ad944ff2724960f29c13ae3d16ceec73e14ec
Lucian <lucianjosegoncales@gmail.com>
2025-11-12 08:24:48 -0300

    Exercicio 3 - Curso 5 - JavaScript - objetos

---
+++ receita.js
@@ -0,0 +1,7 @@
+const receita = {
+    nome: 'Receita 1',
+    ingredientes: ['fermento', 'farinha de trigo', 'sal'],
+    tempoPreparo: 2
+};
+
+console.log("Ingrediente complementar:", receita.ingredientes[1]);
